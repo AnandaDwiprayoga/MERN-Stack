7617be6d059bf8806db11d0a1a95bcaf2634ae2e
AnandaDwiPrayoga <Prayogaananda04@gmail.com>
2020-04-30 13:25:33 +0700

    finish mostpicked elements and arrange to landingpage

--- src/pages/LandingPages.js
+++ src/pages/LandingPages.js
@@ -3,13 +3,22 @@ import React, { Component } from "react";
 import Header from "parts/Header";
 import Hero from "parts/Hero";
 import landingPage from "json/landingPage.json";
+import MostPicked from "parts/MostPicked";
 
 export default class LandingPages extends Component {
+  constructor(props) {
+    super(props);
+    this.refMostPicked = React.createRef();
+  }
   render() {
     return (
       <>
         <Header {...this.props}></Header>
-        <Hero data={landingPage.hero} />
+        <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
+        <MostPicked
+          refMostPicked={this.refMostPicked}
+          data={landingPage.mostPicked}
+        />
       </>
     );
   }
